15f0fdb3a3d376e32560b915bdbe879ca3dd9105
Nalin <nalin.2nextgen@gmail.com>
2022-03-25 12:54:49 +0530

    Added Home Page.

--- event_manager/lib/main.dart
+++ event_manager/lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:event_manager/pages/Home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
     title: "Event Manager",
+    home: const Home(),
+    theme: ThemeData(primarySwatch: Colors.purple),
   ));
 }

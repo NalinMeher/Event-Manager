996c931dd8a69f0171a1c092859c4047b3cb9063
Nalin <nalin.2nextgen@gmail.com>
2022-03-30 21:23:10 +0530

    Added Bottom Navigation Bar, with onTap stateChange.

--- event_manager/lib/pages/Home.dart
+++ event_manager/lib/pages/Home.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _selectedIndex = 0;
+
+  static TextStyle optionStyle =
+  const TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  final List<Widget> _widgetOptions = <Widget>[
+    Text("1: Home", style: optionStyle),
+    Text("2: Business", style: optionStyle),
+    Text("3: School", style: optionStyle),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -9,9 +24,34 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Event Manager"),
       ),
-      body: const Center(
-        child: Text("Home"),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        // selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
       ),
     );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 }

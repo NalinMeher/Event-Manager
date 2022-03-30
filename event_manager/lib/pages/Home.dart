c62beadee5ed112b82d94601ae95c2e14285f1d6
Nalin <nalin.2nextgen@gmail.com>
2022-03-30 12:08:16 +0530

    Home Page UI design.

--- event_manager/lib/pages/Home.dart
+++ event_manager/lib/pages/Home.dart
@@ -1,16 +1,42 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Event Manager"),
       ),
-      body: const Center(
-        child: Text("Home"),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return Card(
+            child: Padding(
+              padding: const EdgeInsets.only(
+                  top: 32, bottom: 32, left: 16, right: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "Note Title",
+                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    "Note Text",
+                    style: TextStyle(color: Colors.grey.shade600),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+        itemCount: 50,
       ),
     );
   }

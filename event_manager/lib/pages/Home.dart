e24abb1e726a1dbb4fa32a45d8f62041cb66a8ff
Nalin <nalin.2nextgen@gmail.com>
2022-03-30 11:49:34 +0530

    Simple SharedPreference Practice, count the number of app startup.

--- event_manager/lib/pages/Home.dart
+++ event_manager/lib/pages/Home.dart
@@ -1,7 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  static const keyStartupNumber = "startupNumber";
+
+  String _haveStarted3Times = "";
+
+
+  @override
+  void initState() {
+    super.initState();
+    _incrementStartup();
+  }
+
+  Future<int> _getIntFromSharedPref() async {
+    final prefs = await SharedPreferences.getInstance();
+    final startupNumber = prefs.getInt(keyStartupNumber);
+    if (startupNumber == null) {
+      return 0;
+    }
+    return startupNumber;
+  }
+
+  Future<void> _resetCounter() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setInt(keyStartupNumber, 0);
+  }
+
+  Future<void> _incrementStartup() async {
+    final prefs = await SharedPreferences.getInstance();
+    int lastStartupNumber = await _getIntFromSharedPref();
+    int currentStartupNumber = ++lastStartupNumber;
+    await prefs.setInt(keyStartupNumber, currentStartupNumber);
+
+    if (currentStartupNumber == 3) {
+      setState(
+          () => _haveStarted3Times = '$currentStartupNumber Times Completed');
+      await _resetCounter();
+    } else {
+      setState(() {
+        _haveStarted3Times = "$currentStartupNumber Times started the app";
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,8 +57,11 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Event Manager"),
       ),
-      body: const Center(
-        child: Text("Home"),
+      body: Center(
+        child: Text(
+          _haveStarted3Times,
+          style: const TextStyle(fontSize: 32),
+        ),
       ),
     );
   }
